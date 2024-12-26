6ddf82302a19cdc41d65a9f8ba830f179cfd90b4
Yunusov321 <diloromsaidova93@gmail.com>
2024-12-26 18:44:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let ot = prompt("Ismingizni kiriting")
+
+let space = prompt("space di idini kiriting")
+
+let password = prompt("space dagi passwordingizni kiriting")
+
+let all = {
+    name: `${ot}`,
+    id: `${space}`,
+    password: `${password}`
+};
+
+console.log(all);
